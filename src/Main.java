b4e7fd40b48d786b2b3e89178a8be2b785076542
Juanekko <bonshy.29.03@gmail.com>
2022-12-15 10:30:17 +0100

    Tablero y posicion 2

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,34 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        String[][] tablero = tablero();
+        mostrarTablero(tablero);
+        String i = posicion(tablero);
+        System.out.println(i);
 
-
+    }
+    public static void mostrarTablero(String[][] array){
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < array.length; j++){
+                System.out.print(" " + array[i][j]);
+            }
+            System.out.println();
+        }
     }
     public static  String[][] tablero(){
         String[][] tablero=new String[8][8];
         for (int i=7;i>-1;i--){
             for (int j=0;j<tablero[i].length;j++){
                 String l="abcdefgh";
-                char letra=l.charAt(i+1);
+                char letra=l.charAt(j);
                 tablero[i][j]=letra+""+(i+1);
             }
         }
         return tablero;
     }
-    public  static  int posicion(String[][] tab) {
+    public  static  String posicion(String[][] tab) {
         Scanner SC = new Scanner(System.in);
         System.out.println("Introduce la posicion deseada");
         String pos = SC.next();
@@ -24,13 +36,19 @@ public class Main {
         int posj=0;
         for (int i = 0; i < tab.length; i++) {
             for (int j = 0; j < tab[i].length; j++) {
-                if (pos.equals(tab[i][j])) {
+                if (Objects.equals(tab[i][j], pos)){
                     posi=i;
                     posj=j;
                 }
             }
         }
-        String p=posi+""+posj;
-        return Integer.getInteger(p);
+        return posi+""+posj;
+
     }
+    /*public static String[][]PosiblesMovimientos(String[][] tab,String pieza, int pos){
+        if (pieza == "P") {
+            String posicionPeon=PosicionPeon(tab, pos);
+        }
+        if (pieza ==)
+    }*/
 }

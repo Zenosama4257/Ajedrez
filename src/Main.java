adecc17b00cc28554aa3dc65b49a96c9555c7d39
IsmaMoreno <xismahackerx@gmail.com>
2022-12-15 09:39:47 +0100

    no se que poner

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
 
     }
-
-    public static void mostrarTablero(){
-
-    }
-
     public static  String[][] tablero(){
         String[][] tablero=new String[8][8];
-        for (int i=7;i>=-1;i--){
+        for (int i=7;i>-1;i--){
             for (int j=0;j<tablero[i].length;j++){
                 String l="abcdefgh";
                 char letra=l.charAt(i+1);
@@ -18,5 +15,22 @@ public class Main {
             }
         }
         return tablero;
+    }
+    public  static  int posicion(String[][] tab) {
+        Scanner SC = new Scanner(System.in);
+        System.out.println("Introduce la posicion deseada");
+        String pos = SC.next();
+        int posi=0;
+        int posj=0;
+        for (int i = 0; i < tab.length; i++) {
+            for (int j = 0; j < tab[i].length; j++) {
+                if (pos.equals(tab[i][j])) {
+                    posi=i;
+                    posj=j;
+                }
+            }
+        }
+        String p=posi+""+posj;
+        return Integer.getInteger(p);
     }
 }

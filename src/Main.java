63a78909c415eaa4540b8aeaa1e6841645551d3c
Zenosama4257 <javiertomasvicente4257@gmail.com>
2022-12-19 07:27:04 +0100

    MovimientosAlfil y torre

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner( System.in);
         String  [][] Tablero = new String[9][9];
-        int contador = 0;
+       
 
 
 
@@ -51,5 +51,74 @@ public class Main {
             }
         }
         return posmov;
+    }
+
+
+
+    public static String [] movimientosAlfil (String[][] tab, String posicion) {
+        char pos1 = posicion.charAt(0);
+        String f = String.valueOf(pos1);
+        char pos2 = posicion.charAt(1);
+        String c = String.valueOf(pos2);
+        int posf = Integer.parseInt(f);
+        int posc = Integer.parseInt(c);
+        int auxf = posf;
+        int auxc = posc;
+        String[] posmov = new String[13];
+        int count = 0;
+        for (int i = 0; i < 7; i++) {
+            auxc++;
+            auxf++;
+            String aux = String.valueOf(auxf) + String.valueOf(auxc);
+            if ( (auxc <= 7) && (auxf <= 7)) {
+                posmov[count] = aux;
+                count++;
+            }
+
+
+        }
+        auxf = posf;
+        auxc = posc;
+
+        for (int j = 0; j < 7; j++) {
+            auxc++;
+            auxf--;
+            String aux = String.valueOf(auxf) + String.valueOf(auxc);
+            if ((auxc <= 7) && (auxf >= 0)) {
+                posmov[count] = aux;
+                count++;
+            }
+
+        }
+        auxf = posf;
+        auxc = posc;
+        for (int u = 0; u < 7; u++) {
+            auxc--;
+            auxf++;
+            String aux = String.valueOf(auxf) + String.valueOf(auxc);
+            if ( (auxc >= 0) && (auxf <= 7)) {
+                posmov[count] = aux;
+                count++;
+            }
+
+        }
+        auxf = posf;
+        auxc = posc;
+        for (int p = 0; p < 7; p++) {
+            auxc--;
+            auxf--;
+            String aux = String.valueOf(auxf) + String.valueOf(auxc);
+            if ((auxc >= 0) && (auxf >= 0)) {
+                posmov[count] = aux;
+                count++;
+            }
+        }
+
+        String [] devuelve = new String[count];
+        for (int d= 0; d< count; d++) {
+            devuelve[d] = posmov[d];
+        }
+        return devuelve;
+
     }
 }

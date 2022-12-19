a39bd1b3bc09b654e2e115291f774766737b56bf
Juanekko <bonshy.29.03@gmail.com>
2022-12-19 10:04:53 +0100

    Tablero, posicion y posibles movimientos arreglados con errores y peon

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import org.w3c.dom.ls.LSOutput;
-
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -7,9 +6,9 @@ public class Main {
     public static void main(String[] args) {
         String[][] tablero = tablero();
         mostrarTablero(tablero);
-        String i = posicion(tablero);
-        System.out.println(i);
-
+        String pos="h5";
+        String[] Movimientos=PosiblesMovimientos(tablero,pos);
+        System.out.println(Arrays.toString(Movimientos)+"lOS POSIBLE MOV");
     }
     public static void mostrarTablero(String[][] array){
         for (int i = 0; i < array.length; i++){
@@ -32,7 +31,6 @@ public class Main {
     }
     public  static  String posicion(String[][] tab) {
         Scanner SC = new Scanner(System.in);
-
         String pos="";
         boolean b=true;
         do {
@@ -41,15 +39,16 @@ public class Main {
 
                 pos= SC.next();
                 for (int i=0;i<pos.length();i++){
-                    char c1=pos.charAt(i);
-                    char c2=pos.charAt(i++);
-                    if (c1<'a'||c1>'h') throw (new IndexOutOfBoundsException());b=false;
-                    if (c2<'1'||c2>'8') throw (new IndexOutOfBoundsException());b=false;
+                    char c1=pos.charAt(0);
+                    char c2=pos.charAt(1);
+                    if (c1<'a'||c1>'h') throw (new IndexOutOfBoundsException());
+                    if (c2<'1'||c2>'8') throw (new IndexOutOfBoundsException());
                 }
             }catch (IndexOutOfBoundsException e){
+                b=false;
                 System.out.println("No has introducido una posicion valida");
             }
-        }while (b);
+        }while (!b);
         int posi=0;
         int posj=0;
         for (int i = 0; i < tab.length; i++) {
@@ -62,33 +61,167 @@ public class Main {
         }
         return posi+""+posj;
     }
-    public static int[] PosiblesMovimientos(String[][] tab,String pieza, String pos){
-        if (Objects.equals(pieza, "P")) {
-            /*int[] posicionPeon=PosicionPeon(tab, pos);
-            return posicionPeon;*/
+    public static String[] PosiblesMovimientos(String[][] tab,/*String pieza,*/ String pos){
+        return PosicionPeon(tab, pos);
+        /*if (Objects.equals(pieza, "P")) {
+            return PosicionPeon(tab, pos);
         }
         if (Objects.equals(pieza, "T")) {
-            /*int[] posicionTorre=PosicionTorrre(tab, pos);
-            return posicionTorre;*/
+            int[] posicionTorre=PosicionTorrre(tab, pos);
+            return posicionTorre;
         }
         if (Objects.equals(pieza, "C")){
-            /*int[] posicionCaballo=PosicionCaballo(tab, pos);
-            return posicionCaballo;*/
+            int[] posicionCaballo=PosicionCaballo(tab, pos);
+            return posicionCaballo;
         }
         if (Objects.equals(pieza, "A")){
-            /*int[] posicionAlfil=PosicionAlfil(tab, pos);
-            return posicionAlfil;*/
+            int[] posicionAlfil=PosicionAlfil(tab, pos);
+            return posicionAlfil;
         }
         if (Objects.equals(pieza, "D")){
-            /*int[] posicionTorre=PosicionTorre(tab, pos);
+            int[] posicionTorre=PosicionTorre(tab, pos);
             int[] posicionAlfil=PosicionAlfil(tab, pos);
             int[] posionDama=posicionTorre+posicionAlfil;
-            return posionDama;*/
+            return posionDama;
         }
         if (Objects.equals(pieza, "R")){
-            /*int[] posicionRey=PosicionRey(tab, pos);
-            return posicionRey;*/
+            int[] posicionRey=PosicionRey(tab, pos);
+            return posicionRey;
         }
-        return new int[]{0};
+        return new String[]{""};*/
+    }
+    public static String[] PosicionPeon(String[][] tab, String pos) {
+        String[] MovimientosPeon = new String[4];
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < tab.length; i++) {
+            for (int j = 0; j < tab[i].length; j++) {
+                if (Objects.equals(tab[i][j], pos)) {
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        Scanner sc = new Scanner(System.in);
+        String color="";
+        do {
+            System.out.println("Introduce si es B (blanca) o N (negra)");
+            color = sc.next();
+        }while (color=="B"||color=="N");
+        int cont =0;
+        if (color.equalsIgnoreCase("B")) {
+            if (x == 7) {
+                System.out.println("Este peon no puede estar aqui porfavor intoduzca una adecuada");
+                posicion(tab);
+            }
+            try {
+                cont +=1;
+                MovimientosPeon[0] = tab[x - 1][y - 1];
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+                cont -=1;
+            }
+            try {
+                cont +=1;
+                MovimientosPeon[1] = tab[x - 1][y];
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+                cont -=1;
+            }
+            try {
+                cont +=1;
+                MovimientosPeon[2] = tab[x - 1][y + 1];
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+                cont -=1;
+            }
+            try {
+                if (x == 6) {
+                    cont +=1;
+                    MovimientosPeon[3] = tab[x - 2][y];
+                }
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+                cont -=1;
+            }
+            String[] PosicionPeon=new String[cont];
+            if (cont==4){
+                for (int i=0; i<PosicionPeon.length; i++){
+                    PosicionPeon[i] = MovimientosPeon[i];
+                }
+            }
+            if (cont==3){
+                for (int i=0;i< PosicionPeon.length; i++){
+                    PosicionPeon[i]=MovimientosPeon[i];
+                }
+            }
+            int j=0;
+            if (cont==2){
+                for (int i=0;i< MovimientosPeon.length-2; i++){
+                    String n=MovimientosPeon[i];
+                    if (Objects.nonNull(n)){
+                        PosicionPeon[j]=MovimientosPeon[i];
+                        j++;
+                    }
+
+                }
+            }
+            return PosicionPeon;
+
+
+
+        }
+        if (color.equalsIgnoreCase("N")) {
+            if (x == 0) {
+                System.out.println("Este peon no puede estar aqui porfavor intoduzca una adecuada");
+                posicion(tab);
+            }
+            try {
+                cont +=1;
+                MovimientosPeon[0] = tab[x + 1][y - 1];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                cont -=1;
+            }
+            try {
+                cont +=1;
+                MovimientosPeon[1] = tab[x + 1][y];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                cont -=1;
+            }
+            MovimientosPeon[2] = tab[x + 1][y + 1];
+            try {
+                if (x == 1) {
+                    cont+=1;
+                    MovimientosPeon[3] = tab[x + 2][y];
+
+                }
+
+            } catch (ArrayIndexOutOfBoundsException e1) {
+                cont -=1;
+            }
+            String[] PosicionPeon=new String[cont];
+            if (cont==4){
+                for (int i=0; i<PosicionPeon.length; i++){
+                    PosicionPeon[i] = MovimientosPeon[i];
+                }
+            }
+            if (cont==3){
+                for (int i=0;i< PosicionPeon.length; i++){
+                    PosicionPeon[i]=MovimientosPeon[i];
+                }
+            }
+            int j=0;
+            if (cont==2){
+                for (int i=0;i< MovimientosPeon.length-2; i++){
+                    String n=MovimientosPeon[i];
+                    if (Objects.nonNull(n)){
+                        PosicionPeon[j]=MovimientosPeon[i];
+                        j++;
+                    }
+
+                }
+            }return PosicionPeon;
+        }
+        return MovimientosPeon;
     }
 }
